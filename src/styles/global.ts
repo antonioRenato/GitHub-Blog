6138058730a0698ea64c7edaa3d3cbd365bcc833
antonioRenato <antoniorpfs@gmail.com>
2023-07-05 22:11:26 -0300

    create components and page

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,13 +5,20 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
 
     :focus {
       outline: 0;
       background-color: ${props => props.theme["base-input"]};
     }
 
+    body {
+      background-color: ${props => props.theme["base-background"]};
+      color: ${props => props.theme["base-text"]};
+      -webkit-font-smoothing: antialiased;
+    }
+
     body, input, textarea, button {
       font: 400 1rem Nunito, sans-serif;
     }
-`;
+`
