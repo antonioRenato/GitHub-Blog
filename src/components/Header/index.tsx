7b881854e4122cef7e5770b502fc8ab063f1f2da
antonioRenato <antoniorpfs@gmail.com>
2023-07-07 21:52:35 -0300

    header finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
-import { HeaderContainer, HeaderContent } from "./styles";
+import { HeaderContainer, LogoImage, SideImages } from "./styles";
+import logo from '../../assets/logo.svg'
+import imageRigth from '../../assets/headerRigth.svg'
+import imageLeft from '../../assets/headerLeft.svg'
+
 
 export function Header() {
   return (
     <HeaderContainer>
-      <HeaderContent />
+      <SideImages>
+        <img src={imageLeft} alt="" />
+        <LogoImage>
+          <img src={logo} alt="" />
+        </LogoImage>  
+        <img src={imageRigth} alt="" />
+      </SideImages>         
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,10 +2,25 @@ import { styled } from "styled-components";
 
 export const HeaderContainer = styled.header`
   background: linear-gradient(180deg, ${props => props.theme["base-profile"]} 25%, ${props => props.theme.blue} 550%);
-  padding: 1.5rem;
   height: 18.75rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
-export const HeaderContent = styled.div`
+export const LogoImage = styled.div`
+  width: 9.25rem;
+  height: 6.125rem;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 1rem;
+`
+
+export const SideImages = styled.div`
+  display: flex;
+  justify-content: space-between;
   width: 100%;
-  max-width: 1120px;  
 `
